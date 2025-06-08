26d3cfaf18723ba37f7231d73a0a89ff50778961
vmaguiar <vmaguiar@hotmail.com>
2025-06-08 19:08:56 -0300

    fix: changes for SFML 3

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,23 @@
 
 int main(){
     // Config Tela
-    const int SCREEN_WIDTH = 1920;
-    const int SCREEN_HEIGHT = 1080;
-    const int FPS = 60;
+    constexpr int SCREEN_WIDTH = 1920;
+    constexpr int SCREEN_HEIGHT = 1080;
+    constexpr int FPS = 60;
 
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, 32), "SFML WORKS!!");
+    sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH,SCREEN_HEIGHT}), "SFML WORKS!!");
     window.setFramerateLimit(FPS);
 
 
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                    window.close();
+                }
             }
         }
 
