df8b68e67d2152d33150616c00557dcc4fb58636
vmaguiar <vmaguiar@hotmail.com>
2025-06-09 16:28:07 -0300

    feat: stars are now rendered in depth order

--- src/star.cpp
+++ src/star.cpp
@@ -1,4 +1,6 @@
 #include "star.hpp"
+
+#include <algorithm>
 #include <iostream>
 
 std::vector<Star> Star::initializateStars(uint16_t contador) {
@@ -13,16 +15,21 @@ std::vector<Star> Star::initializateStars(uint16_t contador) {
 
         stars.push_back({{x, y}, z, (1.0f / z)});
     }
+    // Sort by depth
+    std::sort(stars.begin(), stars.end(), [](const Star &starA, const Star &starB) {
+        return starA.starDepth > starB.starDepth;
+    });
     return stars;
 }
 
-void Star::renderAndDrawStars(const std::vector<Star> &stars, sf::RenderWindow &window) {
+void Star::renderAndDrawStars(std::vector<Star> &stars, sf::RenderWindow &window) {
     sf::CircleShape shape{config::starRadius};
-    for (const Star &star: stars) {
+    for (Star &star: stars) {
         if (star.starDepth > config::zDepthMin) {
-            const sf::Vector2f tempPosition = star.position * star.starScale + (config::windowSize_f * 0.5f);
-            shape.setPosition(tempPosition);
+            shape.setPosition(star.position * star.starScale + (config::windowSize_f * 0.5f));
             shape.setScale({star.starScale, star.starScale});
+            star.setStarColor();
+            shape.setFillColor({star.starColor, star.starColor, star.starColor});
             window.draw(shape);
         }
     }
@@ -33,6 +40,12 @@ void Star::makeStarsTravel(std::vector<Star> &stars) {
         star.starDepth = star.starDepth - (config::cameraSpeed * config::configDeltaTime);
         star.starScale = 1.0f / star.starDepth;
     }
+}
+
+void Star::setStarColor() {
+    this->depthRatio = (this->starDepth - config::zDepthMin) / (config::zDepthMax - config::zDepthMin);
+    this->colorRatio = 1.0f - this->depthRatio;
+    this->starColor = static_cast<uint8_t>(this->colorRatio * 255.0f);
 }
 
 void Star::setStarPosition(sf::Vector2f position) {
--- src/star.hpp
+++ src/star.hpp
@@ -8,12 +8,17 @@ struct Star {
     sf::Vector2f position;
     float starDepth = 1.0f;
     float starScale = 1.0f;
+    float depthRatio = 1.0f;
+    float colorRatio = 1.0f;
+    uint8_t starColor = 255;
 
     static std::vector<Star> initializateStars(uint16_t contador);
 
-    static void renderAndDrawStars(const std::vector<Star> &stars, sf::RenderWindow &window);
+    static void renderAndDrawStars(std::vector<Star> &stars, sf::RenderWindow &window);
 
     static void makeStarsTravel(std::vector<Star> &stars);
+
+    void setStarColor();
 
     void setStarPosition(sf::Vector2f position);
 };
