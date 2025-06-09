109ca886a9af3d2f695713607947566bc79bad1f
vmaguiar <vmaguiar@hotmail.com>
2025-06-09 11:30:57 -0300

    refactor: changes star render responsibility

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,18 @@
 #include "star.hpp"
 
 int main() {
-    auto window = sf::RenderWindow(sf::VideoMode(config::windowSize), "Stars Infinity");
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode(config::windowSize), "Stars Infinity");
     window.setFramerateLimit(config::maxFramerate);
 
     // Stars Creation
-    std::vector<Star> stars = Star::initializateStars(config::starsCount, config::windowSize.x, config::windowSize.y);
+    std::vector<Star> stars = Star::initializateStars(config::starsCount);
 
     while (window.isOpen()) {
         processEvents(window);
 
         window.clear();
         // Render...
-        sf::CircleShape shape{config::starRadius};
-        for (Star const &star: stars) {
-            shape.setPosition(star.position);
-            window.draw(shape);
-        }
+        Star::renderAndDrawStars(stars, window);
         window.display();
     }
 }
